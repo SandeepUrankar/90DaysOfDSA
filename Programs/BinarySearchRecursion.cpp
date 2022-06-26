fde84ce1ec92dd0638ae1c27770970666a55e580
SandeepUrankar <sandeepurankar.98@gmail.com>
2022-06-27 00:45:14 +0530

    Day 4.

--- Programs/BinarySearchRecursion.cpp
+++ Programs/BinarySearchRecursion.cpp
@@ -6,19 +6,28 @@ int num[] = {5,6,7,8};
 int mid;
 int bs(int start, int end, int key)
 {
+    //If the key not found return -1.
     if(start > end)
         return -1;
+    //Compute mid.
     mid = (start + (end - start)) / 2;
+    //Key found at mid.
     if(num[mid] == key)
         return mid;
-    else if(key > num[mid])
-        return bs(mid+1, end, key);
-    else if(key < num[mid])
+    //Key is in the right sub array.
+    if(key > num[mid])
+        return bs(mid + 1, end, key);
+    //Key is in the left sub array.
+    else
         return bs(start, mid - 1, key);
 }
 int main()
 {
-    int ans = bs(0,3,2);
-    cout<<"5 is at "<<ans;
+    int ans = bs(0,3,8);
+    if(ans == -1)
+        cout<<"Key not found"<<endl;
+    else
+        cout<<"5 is at "<<ans<<endl;
+    return 0;
 }
 
