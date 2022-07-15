d0ce6d75549f740c423e676757fa8d30ab001812
SandeepUrankar <sandeepurankar.98@gmail.com>
2022-07-15 22:40:02 +0530

    Added insertLast and insert method.

--- src/Programs/LinkedList/Day_21/DLL.java
+++ src/Programs/LinkedList/Day_21/DLL.java
@@ -3,33 +3,80 @@ package Programs.LinkedList.Day_21;
 public class DLL {
     int size;
     Node head;
-    Node tail;
 
-    public DLL(){
+    public DLL() {
         size = 0;
     }
 
-    public void insertFirst(int val){
-        Node node = new Node(val);
+    public void insertFirst(int data) {
+        Node node = new Node(data);
         node.next = head;
         node.prev = null;
-        if (head != null){
+        if (head != null) {
             head.prev = node;
         }
         head = node;
         size++;
     }
 
-    public void display(){
+    public void insertLast(int data) {
+        Node node = new Node(data);
+        node.next = null;
+        Node last = head;
+
+        if (head == null) {
+            head = node;
+            node.prev = null;
+            return;
+        }
+
+        while (last.next != null) {
+            last = last.next;
+        }
+
+        last.next = node;
+        node.prev = last;
+        size ++;
+    }
+
+    public Node find(int data){
         Node temp = head;
         while(temp != null){
-            System.out.print(temp.data+"->");
+            if (temp.data == data){
+                return temp;
+            }
+            temp = temp.next;
+        }
+        return null;
+    }
+
+    public void insert(int after, int data){
+        Node prev = find(after);
+        if (prev == null){
+            System.out.println("Value "+after+" doesn't exist.");
+            return;
+        }
+
+        Node node = new Node(data);
+        node.next = prev.next;
+        prev.next = node;
+        node.prev = prev;
+        if(node.next != null){
+            node.next.prev = node;
+        }
+        size++;
+    }
+
+    public void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
             temp = temp.next;
         }
         System.out.println("END");
     }
 
-    private class Node{
+    private class Node {
         int data;
         Node next;
         Node prev;
--- src/Programs/LinkedList/Day_21/Main.java
+++ src/Programs/LinkedList/Day_21/Main.java
@@ -5,6 +5,11 @@ public class Main {
         DLL list = new DLL();
         list.insertFirst(12);
         list.insertFirst(11);
+        list.insertLast(77);
+        list.insertLast(88);
+        list.insert(77, 78);
+        list.insert(88, 99);
+        list.insert(99, 99);
         list.display();
         System.out.println(list.size);
     }
